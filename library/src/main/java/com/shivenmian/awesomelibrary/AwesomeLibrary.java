c5d0b58e45c4f9e45c0304e8a80e71f09b814108
Shiven Mian <shiven15094@iiitd.ac.in>
2016-07-03 15:22:41 +0530

    implemented lib code

---
+++ library/src/main/java/com/shivenmian/awesomelibrary/AwesomeLibrary.java
@@ -0,0 +1,22 @@
+package com.shivenmian.awesomelibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by shivenmian on 03/07/16.
+ */
+public class AwesomeLibrary {
+    private static AwesomeLibrary ourInstance = new AwesomeLibrary();
+
+    public static AwesomeLibrary getInstance() {
+        return ourInstance;
+    }
+
+    private AwesomeLibrary() {
+    }
+
+    public void makeMeAwesome(Context context, String data){
+        Toast.makeText(context, "Awesome " + data, Toast.LENGTH_SHORT).show();
+    }
+}
